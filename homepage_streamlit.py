f9b02a59a2a1a22f9953a5c963e74305e2a4c9f4
caseyedwardopiela <96747682+caseyedwardopiela@users.noreply.github.com>
2022-09-25 18:26:27 -0700

    Update homepage_streamlit.py

--- homepage_streamlit.py
+++ homepage_streamlit.py
@@ -1,8 +1,17 @@
 import streamlit as st
 
 st.set_page_config(
-  page_title="Multipage App"
+  page_title="All in One Basketball Predictions"
 )
 
-st.title("Main Page")
-st.sidebar.success("Select a page above.")
+st.sidebar.success("Select a page on the side to continue...")
+
+st.title('2022-2023 NBA Fantasy Score Predictions')
+
+st.markdown("""
+This app pulls historical data from Basketball-Reference.com, including college, international, and regular season stats. After compiling everything,
+it runs a combination of several machine learning algorithms to predict all of the traditional stats for each player this coming season. In addition, 
+award predictions have been added as well based on players' expected stats.
+
+##### Last Updated: 25 September 2022
+""")
