27302d2ca64814aee079e7ca597a65ea886d7ca4
caseyedwardopiela <96747682+caseyedwardopiela@users.noreply.github.com>
2022-09-25 19:25:21 -0700

    Update 5_✨Could_I_Be_An_All-Star?.py

--- pages/5_✨Could_I_Be_An_All-Star?.py
+++ pages/5_✨Could_I_Be_An_All-Star?.py
@@ -6,12 +6,12 @@ import numpy as np
 
 allstar_data = pd.read_csv('Data/raw_data_for_allstars.csv').drop(['Unnamed: 0'], axis = 1)
  
-st.title('Team Standings Predictions')
+st.title('Customized All-Star Predictions')
 
 st.markdown("""
 Are you thinking these predictions might be off? Maybe you were wondering how likely your MyCareer player in NBA 2k23
-is to be an All-star. You can input your own stats here for your favorite player to see how likely he is
-to make the All-star game based on your own beliefs.
+is to be an All-Star. You can input your own stats here for your favorite player to see how likely he is
+to make the All-Star game based on your own beliefs.
 """)
 
 st.dataframe(allstar_data)
