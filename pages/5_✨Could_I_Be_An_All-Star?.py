d103669eef88e1cb0a3b3e1e3abbed316db97fad
caseyedwardopiela <96747682+caseyedwardopiela@users.noreply.github.com>
2022-09-25 19:22:08 -0700

    Update 5_✨Could_I_Be_An_All-Star?.py

--- pages/5_✨Could_I_Be_An_All-Star?.py
+++ pages/5_✨Could_I_Be_An_All-Star?.py
@@ -4,23 +4,14 @@ import pandas as pd
 #import matplotlib.pyplot as plt
 import numpy as np
 
-mvps = pd.read_csv('Data/v1_mvp_predictions.csv').drop(['Unnamed: 0'], axis = 1)
-dpoy = pd.read_csv('Data/v1_dpoy_predictions.csv').drop(['Unnamed: 0'], axis = 1)
-roys = pd.read_csv('Data/v1_roy_predictions.csv').drop(['Unnamed: 0'], axis = 1)
+allstar_data = pd.read_csv('Data/raw_data_for_allstars.csv').drop(['Unnamed: 0'], axis = 1)
  
-st.title('Award Winners Predictions')
+st.title('Team Standings Predictions')
 
 st.markdown("""
-Additional models have been run to assign each player's likelihood of winning the league's Most Valuable Player (MVP), Defensive Player of the Year (DPOY),
-and Rookie of the Year (ROY) awards.
+Based on player personnel and my best educated guess, expected standings for both conferences have been assigned to help in calculations
+on subsequent pages.
 """)
 
-award_options = ['MVP', 'DPOY', 'ROY']
-selected_award = st.sidebar.selectbox('For which award would you like to see the most likely players to win it?', award_options) 
+st.dataframe(allstar_data)
 
-if selected_award == 'MVP':
-  st.dataframe(mvps)
-elif selected_award == 'DPOY':
-  st.dataframe(dpoy)
-else:
-  st.dataframe(roys)
