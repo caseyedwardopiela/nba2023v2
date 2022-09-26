c358b51c31cdbf04299ba651bcabe05f5e42b78f
caseyedwardopiela <96747682+caseyedwardopiela@users.noreply.github.com>
2022-09-26 11:50:11 -0700

    Update HomePage.py

--- HomePage.py
+++ HomePage.py
@@ -9,9 +9,10 @@ st.sidebar.success("Select a page above to continue...")
 st.title('2022-2023 NBA Fantasy Score Predictions')
 
 st.markdown("""
-This app pulls historical data from Basketball-Reference.com, including college, international, and regular season stats. After compiling everything,
-it runs a combination of several machine learning algorithms to predict all of the traditional stats for each player this coming season. In addition, 
-award predictions have been added as well based on players' expected stats.
+This app uses the results of a few Python scripts that scraped historic data and used it to predict this NBA season. Historical data was pulled 
+from Basketball-Reference.com, including college, international, and regular season stats. After compiling everything,
+the scripts ran a combination of several machine learning algorithms to estimate all of the traditional stats for each player. In addition, 
+award predictions have been added based on players' expected stats and the teams' expected records.
 
-##### Last Updated: 25 September 2022
+##### Last Updated: 26 September 2022
 """)
